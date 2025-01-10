e37f273160618f3dff8e9dffcd8c09abcd39847c
TheAnh25 <88934332+Twonineh@users.noreply.github.com>
2025-01-10 14:18:27 +0700

    update

--- Librarymanagement/Library-master/src/main/java/com/example/library/repositories/IAuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.library.repositories;
-
-import com.example.library.models.Author;
-import javafx.collections.ObservableList;
-
-public interface IAuthorRepository {
-    ObservableList<Author> getAllAuthor();
-    String getAuthorIdByName(String author);
-}
